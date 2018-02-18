b6f069bde4426775a63a467161891317c7a7b7c1
shamilbi <shamilbi@gmail.com>
2018-02-18 20:37:12 +0700

    app.py: +from db import db, +db.init_app(app)

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, Items
 from resources.store import Store, Stores
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data2.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'f381c12a1cfe98da07700f0c944f9d15d27c78f9d488fc62ff809b60cce0'
 api = Api(app)
+db.init_app(app)
 
 
 @app.before_first_request
@@ -31,6 +33,4 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
     app.run(port=5000, debug=True)
